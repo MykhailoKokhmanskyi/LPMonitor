45ce8fa646e899a0d455f6296c81659bcd635a94
MykhailoKokhmanskyi <mykhailo.kokhmanskyi@gmail.com>
2026-01-29 15:11:14 +0200

    Implemented captcha result response

--- src/controllers/authController.ts
+++ src/controllers/authController.ts
@@ -2,14 +2,19 @@ import type { Request, Response } from 'express';
 import { addAlert } from '../utils/alertHelper.ts';
 import {verifyTurnstile} from '../utils/cfTurnstile.ts';
 
-export const registerForm = (req: Request, res: Response) => {
+export const registerForm = (_req: Request, res: Response) => {
 	res.render('registerForm');
 }
 
 export const register = async (req: Request, res: Response) => {
 	const email = req.body.email
-	const turnstile_result = await verifyTurnstile(req.body['cf-turnstile-response'], req.ip || "")
-	console.log(turnstile_result)
-	addAlert(res, 'success', 'Реєстрація', 'На вашу електронну пошту було надіслано лист з посиланням для закінчення реєстрації. Якщо ви не отримали лист, первірте "спам" або спробуйте ще раз.')
+	const turnstile_valid = process.env.NODE_ENV === 'production' ? (await verifyTurnstile(req.body['cf-turnstile-response'], req.ip || "")).success : true;
+	if(!turnstile_valid) {
+		addAlert(res, 'warning', 'Капча', 'Ви не пройшли перевірку! Будь ласка, спробуйте ще раз.')
+	} else {
+		addAlert(res, 'success', 'Реєстрація', 'На вашу електронну пошту було надіслано лист з посиланням для закінчення реєстрації. Якщо ви не отримали лист, первірте "спам" або спробуйте ще раз.')
+	}
 	res.render('registerForm', { email })
+	
+
 }
