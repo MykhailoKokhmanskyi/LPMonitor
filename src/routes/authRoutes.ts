8298984f661db5075b12c099bf75e1c461a55b06
MykhailoKokhmanskyi <mykhailo.kokhmanskyi@gmail.com>
2026-03-04 13:36:14 +0200

    Implement rate limiting for password resets

--- src/routes/authRoutes.ts
+++ src/routes/authRoutes.ts
@@ -54,8 +54,27 @@ router.post('/login', loginIpRateLimiter, loginEmailRateLimiter, authController.
 
 router.post('/logout', authController.logout)
 
+const resetIpRateLimiter = rateLimit({
+	windowMs: 60*60*1000,
+	max: 4,
+	handler:(req, res, next, options) => {
+		req.flash('alerts', JSON.stringify({title: 'Занадто багато запитів', type: 'warning', msg:'Ви перевищили максимальну дозволену кількість запитів на скидання пароля для цього пристрою. Спробуйте знову через 1 годину.'}))
+		authController.registerForm(req, res)
+	}
+})
+const resetEmailRateLimiter = rateLimit({
+	windowMs: 60*60*1000,
+	max: 3,
+	keyGenerator: (req, _res) => {
+		return req.body.email || ipKeyGenerator(req.ip as string)
+	},
+	handler:(req, res, next, options) => {
+		req.flash('alerts', JSON.stringify({title: 'Занадто багато запитів', type: 'warning', msg:'Ви перевищили максимальну дозволену кількість запитів на скидання пароля з цієї електронної адреси. Спробуйте знову через 1 годину.'}))
+		authController.registerForm(req,res)
+	}
+})
 router.get('/reset-password', authController.resetPasswordForm)
-router.post('/reset-password', authController.resetPassword) //TODO: Implement strict rate limiting
+router.post('/reset-password', resetIpRateLimiter, resetEmailRateLimiter, authController.resetPassword)
 
 router.get('/reset-password/:resetUuid', authController.resetPasswordSecondForm)
 router.post('/reset-password/:resetUuid', authController.resetPasswordSecond)
