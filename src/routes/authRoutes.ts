21b8bd13719005505e30410a01abc782f907c532
Mykhailo Kokhmanskyi <mykhailo.kokhmanskyi@gmail.com>
2026-03-04 13:37:32 +0200

    Refactor(auth):implement rate limiting (#8)
    * refactor: add login rate limiting
    
    * Implement rate limiting for password resets

--- src/routes/authRoutes.ts
+++ src/routes/authRoutes.ts
@@ -30,13 +30,51 @@ router.post('/register', registrationIpRateLimiter, registrationEmailRateLimiter
 router.get('/register/:inviteUuid', authController.registerPasswordForm);
 router.post('/register/:inviteUuid', authController.registerPasswordSubmit);
 
+const loginIpRateLimiter = rateLimit({
+	windowMs: 60*60*1000,
+	max: 4,
+	handler:(req, res, next, options) => {
+		req.flash('alerts', JSON.stringify({title: 'Занадто багато запитів', type: 'warning', msg:'Ви перевищили максимальну дозволену кількість запитів на вхід для цього пристрою. Спробуйте знову через 1 годину.'}))
+		authController.registerForm(req, res)
+	}
+})
+const loginEmailRateLimiter = rateLimit({
+	windowMs: 60*60*1000,
+	max: 3,
+	keyGenerator: (req, _res) => {
+		return req.body.email || ipKeyGenerator(req.ip as string)
+	},
+	handler:(req, res, next, options) => {
+		req.flash('alerts', JSON.stringify({title: 'Занадто багато запитів', type: 'warning', msg:'Ви перевищили максимальну дозволену кількість запитів на вхід з цієї електронної адреси. Спробуйте знову через 1 годину.'}))
+		authController.registerForm(req,res)
+	}
+})
 router.get('/login', authController.loginForm)
-router.post('/login', authController.login) //TODO: Add rate limiting
+router.post('/login', loginIpRateLimiter, loginEmailRateLimiter, authController.login)
 
 router.post('/logout', authController.logout)
 
+const resetIpRateLimiter = rateLimit({
+	windowMs: 60*60*1000,
+	max: 4,
+	handler:(req, res, next, options) => {
+		req.flash('alerts', JSON.stringify({title: 'Занадто багато запитів', type: 'warning', msg:'Ви перевищили максимальну дозволену кількість запитів на скидання пароля для цього пристрою. Спробуйте знову через 1 годину.'}))
+		authController.registerForm(req, res)
+	}
+})
+const resetEmailRateLimiter = rateLimit({
+	windowMs: 60*60*1000,
+	max: 3,
+	keyGenerator: (req, _res) => {
+		return req.body.email || ipKeyGenerator(req.ip as string)
+	},
+	handler:(req, res, next, options) => {
+		req.flash('alerts', JSON.stringify({title: 'Занадто багато запитів', type: 'warning', msg:'Ви перевищили максимальну дозволену кількість запитів на скидання пароля з цієї електронної адреси. Спробуйте знову через 1 годину.'}))
+		authController.registerForm(req,res)
+	}
+})
 router.get('/reset-password', authController.resetPasswordForm)
-router.post('/reset-password', authController.resetPassword) //TODO: Implement strict rate limiting
+router.post('/reset-password', resetIpRateLimiter, resetEmailRateLimiter, authController.resetPassword)
 
 router.get('/reset-password/:resetUuid', authController.resetPasswordSecondForm)
 router.post('/reset-password/:resetUuid', authController.resetPasswordSecond)
